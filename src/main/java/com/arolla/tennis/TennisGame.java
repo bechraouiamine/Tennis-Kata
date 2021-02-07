f3618236d842f683967aebcea66d9ba7fbf2dfa6
bechraoui amine <bechraoui.amine@gmail.com>
2021-02-07 20:40:44 +0100

    Refactoring

--- src/main/java/com/arolla/tennis/TennisGame.java
+++ src/main/java/com/arolla/tennis/TennisGame.java
@@ -8,7 +8,8 @@ import java.util.Map;
  */
 public class TennisGame {
     private static final String SEPARATOR = "-";
-    private static final String PLAYER1 = "Player1";
+    private static final String PLAYER1 = "player1";
+    private static final String PLAYER2 = "player2";
     private static final Map<Integer, String> scores = new HashMap<Integer, String>() {{
         put(0, "love");
         put(1, "fifteen");
@@ -16,6 +17,11 @@ public class TennisGame {
         put(3, "forty");
     }
     };
+    private static final String DEUCE = "deuce";
+    private static final String ADVANTAGE = "advantage";
+    private static final String ALL = "all";
+    private static final String WIN = "win";
+
     private int scoreP1;
     private int scoreP2;
 
@@ -40,24 +46,29 @@ public class TennisGame {
         String result = new String();
         if (scoreP1 <4 && scoreP2 <4 && scoreP1+scoreP2 != 6) {
             if(scoreP2 == scoreP1) {
-                result += scores.get(scoreP1) + SEPARATOR + "all";
+                result += scores.get(scoreP1) + SEPARATOR + ALL;
             } else {
                 result += scores.get(scoreP1) + SEPARATOR + scores.get(scoreP2);
             }
             return result;
-        } else if (scoreP1 >= 3 && scoreP2 >= 3
-                && scoreP1 == scoreP2) {
-            return "deuce";
-        } else if (scoreP1 >= 4 && scoreP1-scoreP2 == 1) {
-            return "advantage-player1";
-        } else if (scoreP2 >= 4 && scoreP2-scoreP1 == 1) {
-            return "advantage-player2";
-        } else if (scoreP1 >= 4 && scoreP1-scoreP2 >= 2) {
-            return "player1-win";
-        } else if (scoreP2 >= 4 && scoreP2-scoreP1 >= 2) {
-                return "player2-win";
+        } else {
+            if (scoreP1 == scoreP2) {
+                result = DEUCE;
+            } else if (Math.abs(scoreP1-scoreP2) == 1) {
+                result = ADVANTAGE;
+                if (scoreP1>scoreP2) {
+                    result += SEPARATOR + PLAYER1;
+                } else {
+                    result += SEPARATOR + PLAYER2;
+                }
+            } else if (Math.abs(scoreP1-scoreP2) >= 2) {
+                if (scoreP1>scoreP2) {
+                    result = PLAYER1 + SEPARATOR + WIN;
+                } else {
+                    result = PLAYER2 + SEPARATOR + WIN;
+                }
+            }
         }
-
-        return "love-fifteen";
+        return result;
     }
 }
